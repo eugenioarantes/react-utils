2ce6c5f5a3e5994b3f4126da9581400f808f601e
Eugenio <eugeniolopesarantesjunior@gmail.com>
2022-10-19 17:54:35 -0300

    creating switch

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import { BlockColor, Button, Container, Switch } from "./styles";
+
 const Home: React.FC = () => {
-  console.log("start");
+  const [status, setStatus] = useState('');
   return (
-    <h1>Hello World</h1>
+    <Container>
+      <Switch>
+        <BlockColor $status={status} />
+        <Button
+          onClick={() => setStatus('No')}
+          $isSelected={status === 'No'}
+          $status={status}
+        >
+          <span>No</span>
+        </Button>
+
+        <Button
+          onClick={() => setStatus('Yes')}
+          $isSelected={status === 'Yes'}
+          $status={status}
+        >
+          <span>Yes</span>
+        </Button>
+
+      </Switch>
+      {console.log(status)}
+    </Container>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,113 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes, Keyframes } from 'styled-components';
+
+interface ButtonProps {
+  $isSelected: boolean;
+  $status: string;
+}
+
+interface BlockColorProps {
+  $status: string;
+}
+
+const leftAnimation = (): Keyframes => keyframes`
+  from {
+    margin-left: 0;
+  }
+  to {
+    margin-left: 50%;
+  }
+`;
+
+const rightAnimation = (): Keyframes => keyframes`
+  from {
+    margin-left: 50%;
+  }
+  to {
+    margin-left: 0;
+  }
+`;
+
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Switch = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+
+  width: 170px;
+  padding: 4px;
+
+  border: 0.5px solid #555555;
+  border-radius: 10px;
+`;
+
+export const Button = styled.button<ButtonProps>`
+  position: relative;
+
+  width: 80px;
+  height: 30px;
+  cursor: pointer;
+
+  color: #555;
+
+  border: 0;
+  border-radius: 10px;
+
+  background: transparent;
+
+  ${({ $isSelected }) =>
+    $isSelected &&
+    css`
+      color: #fff;
+    `};
+
+  ${({ $status }) =>
+    $status === '' &&
+    css`
+     :first-of-type {
+       color: #fff;
+     }
+  `};
+
+  
+  span {
+    transition: color 0.6s;
+    font-weight: bold;
+
+    ${({ $isSelected }) =>
+    !$isSelected &&
+    css`
+        :hover {
+          color: #000;
+        }
+    `};
+    
+  }
+`;
+
+export const BlockColor = styled.div<BlockColorProps>`
+  position: absolute;
+  
+  width: 80px;
+  height: 30px;
+  background: #19c29b;
+
+  border: 0;
+  border-radius: 10px;
+
+  ${({ $status }) => {
+    if ($status === '') return css``;
+
+    return $status === 'No'
+      ? css`
+          animation: ${rightAnimation()} 0.5s forwards;
+        `
+      : css`
+          animation: ${leftAnimation()} 0.5s forwards;
+        `;
+  }}
+`;
