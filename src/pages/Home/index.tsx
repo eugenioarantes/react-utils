227e79da896dd250de336c73998d09d3c51c24f2
Eugenio <eugeniolopesarantesjunior@gmail.com>
2023-01-10 12:23:35 -0300

    using convertPixelToRem

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
+import { Title } from "./styles";
+
 const Home: React.FC = () => {
   console.log("start");
   return (
-    <h1>Hello World</h1>
+    <Title>Hello World</Title>
   );
 };
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,6 @@
-import styled, { css } from 'styled-components';
+import { convertPixelToRem } from 'css-blocks-styled-components';
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: ${convertPixelToRem(25)};
+`;
