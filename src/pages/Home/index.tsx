0eb0daefb9b1a98593b2125875bc62b21b4c7637
Eugenio <eugeniolopesarantesjunior@gmail.com>
2023-01-20 15:20:10 -0300

    adding tasks table in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,74 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+
+import FilterListIcon from '@mui/icons-material/FilterList';
+
 import { Column, Row } from '../../components/Containers';
+import { TASKS } from '../../mocks/Tasks';
+
+import { FilterButton } from './styles';
+import { useToggle } from '../../hooks/toggle';
 
 function Home(): JSX.Element {
+  const { isOn: modalFilterOpen, turnOff: closeFilterModal, turnOn: openFilterModal } = useToggle();
+
+  const hasFilter = true;
+
   return (
     <Column>
-      <Row gap={50}>
-        <h1>Home</h1>
-        <h1>My initial page</h1>
+      <Row justifyContent="space-between">
+        <Typography fontSize={30} align="center">
+          All tasks
+        </Typography>
+
+        <FilterButton $hasFilter={hasFilter} onClick={openFilterModal}>
+          <FilterListIcon />
+        </FilterButton>
       </Row>
 
-      <Row gap={50}>
-        <h1>Other phrase</h1>
-      </Row>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell width={100}>Id</TableCell>
+              <TableCell width={300}>Task name</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {!TASKS.length ? (
+              <TableRow>
+                <TableCell colSpan={4}>
+                  <Typography fontSize={30} align="center">
+                    No tasks found
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            ) : (
+              TASKS.map(({ tasks }) =>
+                tasks.map(({ id, name, status }) => (
+                  <TableRow>
+                    <TableCell>{id}</TableCell>
+
+                    <TableCell>{name}</TableCell>
+
+                    <TableCell>{status}</TableCell>
+                  </TableRow>
+                )),
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Column>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,24 @@
+import IconButton from '@mui/material/IconButton';
+import { getBoxWidthAndHeight } from 'css-blocks-styled-components';
 import styled, { css } from 'styled-components';
+
+interface FilterIconProps {
+  $hasFilter: boolean;
+}
+
+export const FilterButton = styled(IconButton)<FilterIconProps>`
+  svg {
+    color: #666;
+    ${getBoxWidthAndHeight(28)}
+  }
+
+  ${({ $hasFilter }) =>
+    $hasFilter &&
+    css`
+      border: 1px solid #16aeef !important;
+
+      svg {
+        color: #16aeef;
+      }
+    `};
+`;
