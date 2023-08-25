d3e2e5e073996fdf90a0cb6dd122ea632a9525d5
Eugenio <eugeniolopesarantesjunior@gmail.com>
2023-08-25 16:52:58 -0300

    removing console log

--- src/pages/Home/utils.ts
+++ src/pages/Home/utils.ts
@@ -3,8 +3,6 @@ import { isSubstring } from '../../utils/text';
 export function evaluateSearchTerm(task: any, term: string): boolean {
   const { name } = task;
 
-  console.log(name);
-
   const match = [name].some((prop) => isSubstring(prop, term));
 
   return match;
